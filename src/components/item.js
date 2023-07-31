8efd594826a10679c87209c01e319e7c89e4ae9b
praveen12191 <haripraveen816@gmail.com>
2023-07-31 14:06:02 +0530

    updated cart

--- src/components/item.js
+++ src/components/item.js
@@ -1,32 +1,57 @@
-import { useContext } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 import "./css/item.css"
-import { context } from "../App"
-const Item = () => {
-    const fun1=()=>
-    {
+import axios from "axios"
+import { Context } from "./navbar"
+const Item = ({name,img,price,total,settotal}) => {
+    eventP
+    const [val,setval] = useState(0)
+    const {items,setcart,cartCount} = useContext(Context)
+    const fun1=()=>{
         val==0 ? setval(0) : setval(val-1)
+        if(val==0){
+            setval(0)
+        }
+        else{
+            setval(val-1)
+            settotal(total-price)
+        }
     }
-    const fun2=()=>
-    {
+    const fun2=()=>{
         setval(val+1)
+        settotal(total+price)
     }
-    const {val,setval} = useContext(context)
-    return (
-        <div class="top">
-        <div class="cart1">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqSvNZfQIV9FKVAuag9X_dWQfA95pdYQhosQPpnGL_39Br7Jv1Mp6JAIbHZAkYt8jMNlk&usqp=CAU"></img>
-    <div class="quantity">
-        <p>rs:120</p>
-            <button onClick={fun1}class="quantity-btn">-</button>
-        <span >{val}</span>
-        <button onClick={fun2} class="quantity-btn">+</button>
+    const addToCart = () => {
+      
+        // Check if the item already exists in the cart
+        setcart(cartCount+val)
+            let flag = 0 
+           for(let item of items){
+               if(item.name==name)
+               {
+                flag = 1
+                item.count=val}
+           }
+           if(flag==0)
+                items.push({"name":name,"price":price,"count":val})
+      };
+      console.log(total)
+        return (
 
-    </div>
-    <div className="buy">
-        <button>Add to Cart</button>
-    </div>
-</div>
-</div>
+                <div class="top">
+                    <div class="cart1">
+                        <img src={img}></img>
+                    <div class="quantity">
+                        <span>{name}</span>
+                        <p>rs:{price}</p>
+                        <button onClick={fun1} class="quantity-btn">-</button>
+                        <span >{val}</span>
+                        <button onClick={fun2} class="quantity-btn">+</button>
+                    </div>
+                    <div className="buy">
+                        <button onClick={addToCart}>Add to Cart</button>
+                    </div>
+                </div>
+                </div>
 
        );
 }
